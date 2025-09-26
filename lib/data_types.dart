505671a416a95f63379dc8a28a8fe4f4cbda2d2e
Jesse Ezell <jesse.ezell@timu.com>
2025-09-26 14:23:24 -0700

    add view schema / optimize

--- lib/data_types.dart
+++ lib/data_types.dart
@@ -5,7 +5,14 @@ typedef DataTypeConstructor = DataType Function(Map<String, dynamic> data);
 
 /// A global registry mapping a `type` string to a
 /// factory constructor for the corresponding [DataType] subclass.
-final Map<String, DataTypeConstructor> _dataTypes = {};
+final Map<String, DataTypeConstructor> _dataTypes = {
+  "int": (data) => IntDataType.fromJson(data),
+  "date": (data) => DateDataType.fromJson(data),
+  "text": (data) => TextDataType.fromJson(data),
+  "vector": (data) => VectorDataType.fromJson(data),
+  "float": (data) => FloatDataType.fromJson(data),
+  "timestamp": (data) => TimestampDataType.fromJson(data),
+};
 
 /// Abstract base class for data types.
 abstract class DataType {
@@ -41,11 +48,11 @@ class IntDataType extends DataType {
   Map<String, dynamic> toJson() {
     return {'type': 'int'};
   }
-}
 
-/// Register in the global registry
-void _registerIntDataType() {
-  _dataTypes["int"] = (data) => IntDataType.fromJson(data);
+  @override
+  String toString() {
+    return "int";
+  }
 }
 
 /// DateDataType
@@ -63,11 +70,11 @@ class DateDataType extends DataType {
   Map<String, dynamic> toJson() {
     return {'type': 'date'};
   }
-}
 
-/// Register
-void _registerDateDataType() {
-  _dataTypes["date"] = (data) => DateDataType.fromJson(data);
+  @override
+  String toString() {
+    return "date";
+  }
 }
 
 /// FloatDataType
@@ -85,11 +92,33 @@ class FloatDataType extends DataType {
   Map<String, dynamic> toJson() {
     return {'type': 'float'};
   }
+
+  @override
+  String toString() {
+    return "float";
+  }
 }
 
-/// Register
-void _registerFloatDataType() {
-  _dataTypes["float"] = (data) => FloatDataType.fromJson(data);
+/// FloatDataType
+class TimestampDataType extends DataType {
+  TimestampDataType() : super();
+
+  static TimestampDataType fromJson(Map<String, dynamic> data) {
+    if (data['type'] != 'timestamp') {
+      throw Exception("Expected type 'float', got '${data['type']}'");
+    }
+    return TimestampDataType();
+  }
+
+  @override
+  Map<String, dynamic> toJson() {
+    return {'type': 'timestamp'};
+  }
+
+  @override
+  String toString() {
+    return "timestamp";
+  }
 }
 
 /// VectorDataType
@@ -110,11 +139,11 @@ class VectorDataType extends DataType {
   Map<String, dynamic> toJson() {
     return {'type': 'vector', 'size': size, 'element_type': elementType.toJson()};
   }
-}
 
-/// Register
-void _registerVectorDataType() {
-  _dataTypes["vector"] = (data) => VectorDataType.fromJson(data);
+  @override
+  String toString() {
+    return "vector<$elementType>[$size]";
+  }
 }
 
 /// TextDataType
@@ -132,18 +161,9 @@ class TextDataType extends DataType {
   Map<String, dynamic> toJson() {
     return {'type': 'text'};
   }
-}
 
-/// Register
-void _registerTextDataType() {
-  _dataTypes["text"] = (data) => TextDataType.fromJson(data);
-}
-
-/// Helper to register all data types at once.
-void registerAllDataTypes() {
-  _registerIntDataType();
-  _registerDateDataType();
-  _registerFloatDataType();
-  _registerVectorDataType();
-  _registerTextDataType();
+  @override
+  String toString() {
+    return "text";
+  }
 }
