58410b4f173cc26b05b1ccdd76014fc614a5c677
Jesse Ezell <jesse.ezell@timu.com>
2025-10-16 12:21:25 -0700

    fix analyze

--- lib/client.dart
+++ lib/client.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:meshagent/meshagent.dart';
-import 'package:meshagent/participant_token.dart';
 
 enum ProjectRole { member, developer, admin }
 
